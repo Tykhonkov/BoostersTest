e40d4a80e3e3ef5d06cff3d13182e3717bdc6c0f
Ilya <i.tykhonkov@gmail.com>
2020-04-01 13:09:59 +0300

    [Add] time formatter

---
+++ BoostersTestTask/Formatters.swift
@@ -0,0 +1,17 @@
+//
+//  Formatters.swift
+//  BoostersTestTask
+//
+//  Created by Elias on 01.04.2020.
+//  Copyright © 2020 Elias. All rights reserved.
+//
+
+import Foundation
+
+
+let timeFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.dateFormat = "HH:mm a"
+    
+    return formatter
+}()
