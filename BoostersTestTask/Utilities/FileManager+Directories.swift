3b1227af799e1c61ea6d634a6cb4cf5f06d58ed3
Ilya <i.tykhonkov@gmail.com>
2020-04-03 18:03:33 +0300

    [Add] directory utilities

---
+++ BoostersTestTask/Utilities/FileManager+Directories.swift
@@ -0,0 +1,18 @@
+//
+//  FileManager+Directories.swift
+//  BoostersTestTask
+//
+//  Created by Elias on 02.04.2020.
+//  Copyright © 2020 Elias. All rights reserved.
+//
+
+import Foundation
+
+extension FileManager {
+
+    static func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+}
